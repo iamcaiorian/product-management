18a776d51eba7af7282a008ec141389ef8a10d1f
iamcaiorian <caio.rianbr@gmail.com>
2024-11-17 00:06:28 -0300

    refactor: adjust app component structure and define routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductsService } from './services/products/products.service';
+import { Product } from './shared/models/product';
+import { Observable } from 'rxjs';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastService } from './shared/utils/toast.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ProductComponent,
+    CommonModule,
+    NgbToastModule,
+    NgTemplateOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'product-management';
+  toastService = inject(ToastService);
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'products',
+    component: ProductComponent,
+    title: 'Product Management'
+}];
